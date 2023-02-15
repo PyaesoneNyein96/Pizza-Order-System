ed492073bf7f170d3beb571f716979e671bfa6f6
pyaesonenyein <pyaesonenyein.1996@gmail.com>
2023-02-15 21:28:39 +0630

    POS L-34 end

--- resources/views/User/cart/user-card.blade.php
+++ resources/views/User/cart/user-card.blade.php
@@ -107,54 +107,6 @@
 
 @section('script')
 
-    <script>
-        $(document).ready(() => {
-            // Plus
-            $('.btn-plus').click(function() {
-                $parentNode = $(this).parents('tr');
-                calculation();
-
-                // $total Summary
-                summary()
-            })
-            // Minus
-            $('.btn-minus').click(function() {
-                $parentNode = $(this).parents('tr');
-                calculation();
-                // $total Summary
-                summary()
-            });
-
-
-            // Remove
-            $('.removeBtn').click(function() {
-                $parentNode = $(this).parents('tr');
-                $parentNode.remove();
-                // $total Summary
-                $subTotal = 0;
-                summary();
-
-            });
-
-            function summary() {
-                $subTotal = 0;
-                $('#cartTable tr').each(function(i, row) {
-                    $subTotal += Number($(row).find('#hide').val());
-                })
-                $('#subtotal').html(`${$subTotal} Kyats`);
-                $('#final').html(`${$subTotal + 100} Kyats`);
-            }
-
-            function calculation() {
-                $Qty = parseInt($parentNode.find('#quantity').val());
-                $price = Number($parentNode.find('#dbPrice').html().replace('Kyats', ''));
-                $total = $Qty * $price;
-                $parentNode.find('#total').html($total + ' ' + 'Kyats');
-                $parentNode.find('#hide').val($total);
-            }
-
-        })
-    </script>
-
+    <script src="{{ asset('user/js/cart.js') }}"></script>
 
 @endsection
